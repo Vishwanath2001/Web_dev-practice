eadf09ef41e1f6a3447841618512a07b77637ee8
Vishwanath Bhat <76242298+Vishwanath2001@users.noreply.github.com>
2021-10-19 23:41:45 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,18 @@
+var num_of_drum_button=document.querySelectorAll(".drum").length;
+
+for(let i=0;i<num_of_drum_button;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",drumclick);
+}
+
+function drumclick(){
+    //alert("I got clicked");
+ 
+    
+}
+
+
+
+
+
+//var audio=new Audio('sounds/tom-1.mp3');
+  //  audio.play();

7ce0f0a58979650866756e4e7a70e4320e3e3d7a
Vishwanath <vishwanathbhat2001@gmail.com>
2021-12-30 21:01:12 +0530

    click and sound effect added

--- Simon Game Challenge/index.js
+++ Simon Game Challenge/index.js
@@ -1,12 +1,31 @@
 var buttonColors=["red","blue","green","yellow"];
 var gamePattern=[];
+var userClickedPattern=[];
+randomsequence();
 function randomsequence()
 {
     var rand_num = Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[rand_num];
     gamePattern.push(randomChosenColor);
     $('#'+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColor);
+}
+$(".btn").click(function () { 
+    var userChosenColor=$(this).attr('id');
+    userClickedPattern.push(userChosenColor);
+    //console.log(userClickedPattern)
+    playSound(userChosenColor);
+    animatPress(userChosenColor)
+});
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+function animatPress(pressedColor){
+    $('#'+pressedColor).addClass('pressed');
+    setTimeout(function(){
+        $('#'+pressedColor).removeClass('pressed');
+    },100);
+}

2773926bb7e23688eaa1a8384f4dbdad24055419
Vishwanath <vishwanathbhat2001@gmail.com>
2022-01-01 22:13:33 +0530

    tried npm packages

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -1,2 +1,3 @@
-const fs=require("fs");
-fs.copyFileSync("file1.txt","file2.txt");
+var rand_ind_name=require("random-indian-name");
+console.log(rand_ind_name());
+console.log(rand_ind_name({first:true,gender:"male"}));

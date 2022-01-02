71496023f82f6e57682a3a6460aed4a261d30ee8
Vishwanath <vishwanathbhat2001@gmail.com>
2022-01-02 16:21:58 +0530

    post and get method in express

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -4,3 +4,7 @@ app.listen(3000,function () {console.log("Server started at port 3000");  });
 
 app.get("/",function(req,res){res.send('<h1>Hi this is testing my express server</h1>');
 });
+app.get("/contact",function (req,res) {res.send("contact at vishwanathbhat2001@gmail.com");  });
+app.get("/about",function (req,res) {res.send("PAGE OWNED BY VISHWANATH");  });
+app.get("/hobbies",function (req,res) {res.send("Playing games");  });
+

c0e573e6a604d6dd76eda47f5ba3ed86142fd5c4
Vishwanath <vishwanathbhat2001@gmail.com>
2022-01-04 00:10:43 +0530

    Update app.js

--- api/weather-project/app.js
+++ api/weather-project/app.js
@@ -6,8 +6,17 @@ app.get("/",function (req,res) {
     const api_url="https://api.openweathermap.org/data/2.5/find?q=Mysuru&units=metric&appid=5f079803e5af34518c02d08c591af80b";
 
     https.get(api_url,function (api_resp) {
-        console.log(api_resp);
+        
+        //console.log(api_resp);
+        api_resp.on("data",function(data){
+
+            const weather_details=JSON.parse(data);
+            const temperature=weather_details.list[0].main.temp;
+            console.log(temperature);
+            
+
       });
+    });
       res.send("working");
   });
 

2438ba766d9bb2b9fad3830f9345996ad3e6f5d0
Vishwanath <vishwanathbhat2001@gmail.com>
2022-01-03 20:10:30 +0530

    finished express module

--- calculator-challenge/calculator.js
+++ calculator-challenge/calculator.js
@@ -13,3 +13,12 @@ var num2 =Number(req.body.num2);
 var result=num1+num2;
 res.send("Calculation:"+result);
   });
+
+app.get("/bmicalculator",function(req,res){res.sendFile(__dirname+"/bmiCalculator.html");});
+app.post("/bmicalculator",function (req,res) {
+  var weight=parseFloat(req.body.weight);
+  var height=parseFloat(req.body.height);
+  var bmi=weight/(height*height);// just to check
+  res.send("Your BMI is "+bmi);
+
+  });
--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+
 app.listen(3000,function () {console.log("Server started at port 3000");  });
 
 app.get("/",function(req,res){res.send('<h1>Hi this is testing my express server</h1>');

06db2b0d325d9b61337b980f06bc1a8a9abddabd
Vishwanath <vishwanathbhat2001@gmail.com>
2022-01-20 20:49:39 +0530

    finished basic event adding part using ejs

--- todolist/app.js
+++ todolist/app.js
@@ -3,42 +3,35 @@ const bodyParser = require("body-parser");
 
 const app=express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine',"ejs");
 
+
+var items=["Get up","Go to gymm","Eat breakfast"];
 app.get("/",function(req,res){
     var today=new Date();
     var currentDay=today.getDay();
     var day="";
-    switch (currentDay) {
-        case 0:
-            day="Sunday";
-            break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        
-        default:
-            console.log("Error:current day is"+currentDay);
-            break;
-    }
-    res.render('index',{frontDay:day});
+    var options={weekday:'long',
+                 month:'long',
+                 day:'numeric'   
+    
+    };
+    var today=new Date();
+    day=today.toLocaleDateString("en-US",options);
+    res.render('index',{frontDay:day,newItems:items});
     
 
+    
+
+});
+
+app.post("/",function(req,res){
+ var item=req.body.newItem
+ items.push(item);
+
+ res.redirect("/");
 });
 
 app.listen(3000,function(){
